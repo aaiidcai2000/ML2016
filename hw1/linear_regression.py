0fb8a2335d49d2e307bd834e0e479748cab08d8e
aaiidcai2000 <aaiidcai2000@gmail.com>
2016-11-21 13:43:58 +0800

    basic linear_regression result

--- hw1/linear_regression.py
+++ hw1/linear_regression.py
@@ -3,6 +3,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sympy import *
 
+def F(w,b,X):
+    h= lambda x:b+w*x
+    f = np.vectorize(h)
+    return f(X)
 
 def linear_regression(X,Y):
     w,b=symbols('w b')
@@ -19,33 +23,58 @@ def linear_regression(X,Y):
     
     return res[w],res[b]
 
+def plotResult(X,Y,w,b):
+    LR_X = X
+    LR_Y=F(LR_X)
 
-
-#data = pd.read_csv('train.csv',encoding="utf-8")
-data = pd.read_csv('train.csv',encoding="latin1")
-#data = pd.read_csv('train.csv',encoding="gb18030")
-
-in_x = list(map(float,data.iloc[9:190:18,11].tolist()))
-in_y = list(map(float,data.iloc[9:190:18,12].tolist()))
-
-w,b=linear_regression(in_x,in_y)
-
-
-residual = 0
-for i in range(10):
-    residual += (in_y[i]-w*in_x[i]-b)**2
-print('redisual: ',residual,", w: ",w,", b: ",b)
-
-#plot
-LR_X = in_x
-h= lambda x:b+w*x
-F = np.vectorize(h)
-LR_Y=F(LR_X)
-
-plt.plot(LR_X,LR_Y)
-plt.plot(in_x,in_y,'ro')
-plt.show()
+    plt.plot(LR_X,LR_Y)
+    plt.plot(X,Y,'ro')
+    plt.show()
     
+
+def train(data_csv):
+
+    data = pd.read_csv(data_csv,encoding="latin1")
+
+    in_x = list(map(float,data.iloc[9:190:18,11].tolist()))
+    in_y = list(map(float,data.iloc[9:190:18,12].tolist()))
+
+    w,b=linear_regression(in_x,in_y)
+
+    residual = 0
+    for i in range(10):
+        residual += (in_y[i]-w*in_x[i]-b)**2
+    print('redisual: ',residual,", w: ",w,", b: ",b)
+
+    #plotResult(in_x,in_y,w,b)
+    
+    return w,b
+
+w,b=train('train.csv')
+
+data = pd.read_csv('test_X.csv',header=None)
+
+x = list(map(float,data.iloc[9:4320:18,10].tolist()))
+
+y = F(w,b,x) 
+
+res = pd.DataFrame({
+        'id':['id_'+str(i) for i in range(240)],
+        'value':y,
+    })
+print(res)
+
+res.to_csv('linear_regression.csv',index=False )
+
+
+
+
+
+
+
+
+
+
 
 
 
